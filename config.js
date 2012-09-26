b28e8dd89792701b76e0fc0b1f64dd962e743e7d
nomospace <jinlu_hz@163.com>
2012-09-26 11:34:26 +0800

    add app bootstrap framework

---
+++ config.js
@@ -0,0 +1,13 @@
+exports.config = {
+  debug: true,
+  name: 'nodeweibo',
+  description: '简易 weibo 应用',
+  version: 0.1,
+
+  sessionSecret: 'nodeweibo',
+  authCookieName: 'nodeweibo',
+  host: '127.0.0.1',
+  port: 3002
+
+//  db: 'mongodb://127.0.0.1/nodeweibo'
+};
---
+++ routes.js
@@ -0,0 +1,30 @@
+var site = require('./controllers/site');
+
+var express = require('express');
+var path = require('path');
+
+module.exports = function(app) {
+
+  var staticDir = path.join(__dirname, 'public');
+  app.use(express.static(staticDir));
+
+  // url routes
+  app.get('/', site.index);
+  app.get('/signin', site.index);
+
+  // ajax
+  app.get('/ajax/index', site.index);
+
+  app.get('*', function(req, res) {
+    res.render('index.html', {});
+    // throw new NotFound;
+  });
+};
+
+function NotFound(msg) {
+  this.name = 'NotFound';
+  Error.call(this, msg);
+  Error.captureStackTrace(this, arguments.callee);
+}
+
+NotFound.prototype.__proto__ = Error.prototype;
