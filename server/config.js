e3230152f11f7009e59f3bca631fe297afbabf2b
nomospace <jinlu_hz@163.com>
2013-02-05 00:45:19 +0800

    Update api url

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ module.exports = {
   debug: true,
   name: 'waybo',
   description: 'A simple web-weibo base on nodejs.',
-  version: '0.2.2',
+  version: '0.2.3',
 
   app_key: '2645619356',
   app_secret: 'e5f5d38eaa777f00924408686841b5dd',
@@ -11,6 +11,6 @@ module.exports = {
   session_secret: 'waybo',
   auth_cookie_name: 'waybo',
   host: '127.0.0.1',
-  port: 3002,
-  socketPort: 3003
+  port: 3009,
+  socketPort: 3109
 };
